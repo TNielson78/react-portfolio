2f2a7475b652c335120513906b21652bef5c13fd
TNielson78 <travis.nielson01@gmail.com>
2024-04-03 16:10:01 -0600

    maybe fixed images

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,10 +1,10 @@
 import ProjectCard from "../../components/Projectcard";
-import holiday from "/dist/assets/Holiday2-099964c9.png";
-import workday from "/dist/assets/Scheduler-45088492.png";
-import weather from "/dist/assets/weather-0fd870b8.png";
-import workout from "/dist/assets/login_project2-fa924276.png";
-import notes from "/dist/assets/Notes-003ead04.png";
-import passgen from "/dist/assets/Passgen1-34109765.png";
+import Holiday from "../../assets/img/Holiday2.png";
+import Scheduler from "../../assets/img/Scheduler.png";
+import Weather from "../../assets/img/weather.png";
+import Workout from "../../assets/img/login_project2.png";
+import Notes from "../../assets/img/Notes.png";
+import Passgen from "../../assets/img/Passgen1.png";
 import {Row, Col} from "react-bootstrap"
 
 
@@ -14,7 +14,7 @@ const Projects = () => {
             id: 1,
             name: 'Holiday Music Search Engine',
             description: 'A search engine for holiday music that allows users to search for songs by title, artist, or genre. Users can also create an account to save their favorite songs.',  
-            image: holiday,
+            image: Holiday,
             link: 'https://ruthiepina.github.io/Project-1',
             gitLink: 'https://github.com/ruthiepina/Project-1',
         },
@@ -22,7 +22,7 @@ const Projects = () => {
             id: 2,
             name: 'Workday Scheduler',
             description: 'A simple calendar application that allows a user to save events for each hour of the day. This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery.',
-            image: workday,
+            image: Scheduler,
             link: 'https://tnielson78.github.io/Workday-scheduler/',
             gitLink: 'https://github.com/TNielson78/Workday-scheduler',
         },
@@ -30,7 +30,7 @@ const Projects = () => {
             id: 3,
             name: 'Weather Dashboard',
             description: 'A weather dashboard that allows a user to search for a city and view the current weather conditions as well as the 5-day forecast. This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery.',
-            image: weather,
+            image: Weather,
             link: 'https://tnielson78.github.io/Weather-dashboard/',
             gitLink: 'https://github.com/TNielson78/Weather-dashboard',
         },
@@ -38,7 +38,7 @@ const Projects = () => {
             id: 4,
             name: 'Workout Tracker',
             description: 'A workout tracker that allows a user to view, create, and track daily workouts. The user can log multiple exercises in a workout on a given day and track the name, type, weight, sets, reps, and duration of the exercise.',
-            image: workout,
+            image: Workout,
             link: 'https://workout-fitness-tracker-ff2f5d2f6a1e.herokuapp.com/login',
             gitLink: 'https://github.com/hyrumsdolan/fitness-tracker-MVC',
         },
@@ -46,7 +46,7 @@ const Projects = () => {
             id: 5,
             name: 'Note Taker',
             description: 'An application that can be used to write, save, and delete notes. This application uses an express backend and saves and retrieves note data from a JSON file.',
-            image: notes,
+            image: Notes,
             link: 'https://note-taker-tnielson78.herokuapp.com/',
             gitLink: 'https://github.com/TNielson78/Note_taker',
         },
@@ -54,7 +54,7 @@ const Projects = () => {
             id: 6,
             name: 'Password Generator',
             description: 'A password generator that allows a user to create a random password based on criteria they’ve selected. This app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript.',
-            image: passgen,
+            image: Passgen,
             link: 'https://tnielson78.github.io/Password-Generator/',
             gitLink: 'https://github.com/TNielson78/Password-Generator',
         }
